53a7dab7d3e6790acf333472c48a2ac483df87e2
Moazz <moazzemh222@gmail.com>
2023-01-01 23:53:10 +0600

    2nd chapter code

---
+++ main.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for(i=33; i<=126; i++)
+    printf("Ascii value %c is %d\n", i, i);
+    return 0;
+}
